23e77049fb756e2b32370142941ced505b941a56
Conor Campbell <conorc1000@gmail.com>
2020-06-22 16:31:08 +1000

    update server

--- server/src/app.js
+++ server/src/app.js
@@ -2,46 +2,40 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const path = require('path');
-
-const cors = require("cors");
 const aws = require('aws-sdk');
+const dotenv = require('dotenv');
+dotenv.config();
 
 let expressApp = express();
+let app = express();
 expressApp.use(express.static('www'));
 expressApp.set('port', process.env.PORT || 5000);
 const middlewares = require("./middlewares");
 const api = require("./api");
-const app = express();
 
 expressApp.use(morgan("dev"));
-expressApp.use(helmet());
+expressApp.use(helmet()); //helps with cors attacks
 expressApp.use(express.json());
-expressApp.use(cors());
 
-const log = (req, res, next) => {
-  next();
-}
-expressApp.use(log)
+var cors = require('cors')
+expressApp.use(cors())
 
-expressApp.listen(app.get('port'), function () {
+
+expressApp.listen(express().get('port'), function () {
   console.log('Express server listening on port ' + expressApp.get('port'));
-});
-
-expressApp.use(express.static(path.join(__dirname + "/../../", 'build')));
-
-expressApp.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + "/../../", 'build', 'index.html'));
 });
 
 expressApp.get('/sign_s3', function (req, res) {
 
+  console.log("req.query", req.query)
   aws.config.update({
     accessKeyId: process.env.AWS_SECRET_KEY,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
   });
+
   var s3 = new aws.S3();
   var s3_params = {
-    Bucket: 'boatlaunchphotos',
+    Bucket: process.env.S3_PHOTOS_BUCKET,
     Key: 'WebSitePhotos/' + req.query.file_name,
     Expires: 60,
     ContentType: req.query.file_type,
@@ -54,12 +48,18 @@ expressApp.get('/sign_s3', function (req, res) {
     } else {
       var return_data = {
         signed_request: data,
-        url: 'https://' + 'boatlaunchphotos' + '.s3.amazonaws.com/' + req.query.imgId
+        url: 'https://' + process.env.S3_PHOTOS_BUCKET + '.s3.amazonaws.com/' + req.query.file_name
       };
       res.write(JSON.stringify(return_data));
       res.end();
     }
   });
+});
+
+expressApp.use(express.static(path.join(__dirname + "/../../", 'build')));
+
+expressApp.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + "/../../", 'build', 'index.html'));
 });
 
 expressApp.use("/api/v1", api);
