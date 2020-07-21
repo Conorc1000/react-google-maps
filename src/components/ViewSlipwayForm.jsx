ca2d66b04b3211fb991ff424a7316ec79c6afec8
Conor Campbell <conorc1000@gmail.com>
2020-07-21 22:10:12 +1000

    remove tabs

--- src/components/ViewSlipwayForm.jsx
+++ src/components/ViewSlipwayForm.jsx
@@ -230,26 +230,6 @@ class ViewSlipwayForm extends Component {
                 Manage images
               </NavLink>
             </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === "3" })}
-                onClick={() => {
-                  this.toggle("3");
-                }}
-              >
-                Comments
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                className={classnames({ active: this.state.activeTab === "4" })}
-                onClick={() => {
-                  this.toggle("4");
-                }}
-              >
-                Delete
-              </NavLink>
-            </NavItem>
           </Nav>
           <TabContent activeTab={this.state.activeTab}>
             <TabPane tabId="1">
@@ -330,26 +310,6 @@ class ViewSlipwayForm extends Component {
                     <br/>
                     <CardTitle>Current images</CardTitle>
                     {images}
-                  </CardBody>
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId="3">
-              <Row>
-                <Col sm="12">
-                  <CardBody>
-                    <CardTitle>Comments</CardTitle>
-                  </CardBody>
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane tabId="4">
-              <Row>
-                <Col sm="12">
-                  <CardBody>
-                    <CardTitle>Delete slipway or photos</CardTitle>
-
-                    <a href="mailto:conorc1000@gmail.com">Email Me</a>
                   </CardBody>
                 </Col>
               </Row>
